4c0fc68ef373d785a7ac3f959f850ee8ee94213c
rumesh <rumeshk066@gmail.com>
2025-09-07 17:35:53 +0530

    connected db to mongoDB

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/database');
+// const teamRoutes = require('./routes/teamRoutes');
+// const errorHandler = require('./middleware/errorMiddleware');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use('/api/teams', teamRoutes);
+
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
